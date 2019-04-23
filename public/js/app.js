a9f0a2319748201b83572cd73e30e38f6a5bfec8
ujjwalhonmai <37949030+ujjwalhonmai@users.noreply.github.com>
2019-04-23 19:03:35 +0530

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+console.log('Client side javascript file is loaded!')
+const weatherform=document.querySelector('form')
+const search=document.querySelector('input')
+const first=document.querySelector('#first')
+const second=document.querySelector('#second')
+weatherform.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const location=search.value
+    first.textContent='loading...'
+    second.textContent=''
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error)
+            {
+                first.textContent=data.error
+            }
+            else{
+                first.textContent=data.location
+                second.textContent=data.forecast
+            }
+    
+        })
+    })
+    })
